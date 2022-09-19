48fa0c4091c35518e59f09627e787a30a4f6e5fb
Kristine <kristineipolakovai@gmail.com>
2022-09-19 16:15:56 +0300

    counter without ngrx

---
+++ src/app/counter/state/counter.actions.ts
@@ -0,0 +1,5 @@
+import { createAction } from "@ngrx/store";
+
+export const increment = createAction('[Counter Page] increment');
+export const decrement = createAction('[Counter Page] decrement');
+export const reset = createAction('[Counter Page] reset');
